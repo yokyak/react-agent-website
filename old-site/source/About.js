ee454b4953c64f0f1ed91cf507be63ff662ba48b
Andrew Harris <andrewharris@Andrews-MacBook-Pro.local>
2018-01-22 20:29:04 -0800

    changed to another site ui

--- old-site/source/About.js
+++ old-site/source/About.js
@@ -41,6 +41,40 @@ class About extends Component {
               </pre>
             </div>
           </div>
+          <h2>Getting Started</h2>
+          <div id='code-snippets'>
+            <div>
+              <h3>React</h3>
+              <pre className="prettyprint lang-bsh">
+                {'npm install react-agent --save'}
+              </pre>
+              <pre className="prettyprint lang-js">
+                {'import { Agent } from \'react-agent\' \n\n'}
+                {'render(\n'}
+                {'<Agent>\n'}
+                {'  <App />\n'}
+                {'</Agent>\n'}
+                {', document.querySelector(\'#root\'))'}
+              </pre>
+            </div>
+            <div>
+              <h3>Node / Express</h3>
+              <pre className="prettyprint lang-bsh">
+                {'npm install react-agent-server --save'}
+              </pre>
+              <pre className="prettyprint lang-js">
+                {'const agent = require(\'react-agent-server\') \n\n'}
+                {'const server = app.listen(3000)\n\n'}
+                {'const actions = {\n'}
+                {'  getMessages: {\n'}
+                {'    action: \'SELECT * FROM posts\'\n'}
+                {'  }\n'}
+                {'}\n\n'}
+                {'const database = { user: react-agent }\n\n'}
+                {'agent(server, actions, database)'}
+              </pre>
+            </div>
+          </div>
           <h2>The Team</h2>
           <div id='team'>
             <div>
